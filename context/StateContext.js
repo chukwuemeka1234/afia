8655fa8aa61516ae196b25e8f72b52837d3f211c
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-07-19 22:30:35 +0100

    Cart add and remove items

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,4 +1,3 @@
-import product from '@/sanity-afia/schemas/product';
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
 
@@ -38,10 +37,19 @@ export const StateContext = ({ children }) => {
     toast.success(`${qty} ${product.name} added to cart.`);
   }
 
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItems = cartItems.filter((item) => item._id !== product._id);
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice -foundProduct.price * foundProduct.quantity);
+    setTotalQuantities(prevTotalQuantities => prevTotalQuantities - foundProduct.quantity);
+    setCartItems(newCartItems);
+  }
+
   const toggleCartItemQuantity = (id, value) => {
     foundProduct = cartItems.find((item) => item._id === id)
     index = cartItems.findIndex((product) => product._id === id);
-    const newCartItems = cartItems.splice(index, 1)
+    const newCartItems = cartItems.filter((item) => item._id !== id)
 
     if(value === 'inc') {
       setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
@@ -79,7 +87,11 @@ export const StateContext = ({ children }) => {
         incQty,
         decQty,
         onAdd,
-        toggleCartItemQuantity
+        toggleCartItemQuantity,
+        onRemove,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities
       }}  
     >
 
